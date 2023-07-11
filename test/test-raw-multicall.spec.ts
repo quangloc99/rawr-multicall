384ba0c5e64b32b3288d736e6e0fd44e7755487f
Tran Quang Loc <quangloctran99@gmail.com>
2023-07-11 21:14:00 +0700

    Add block number to test data

--- test/test-raw-multicall.spec.ts
+++ test/test-raw-multicall.spec.ts
@@ -6,11 +6,13 @@ import { describeForChain } from './test-helper';
 const TEST_DATA: Record<
     number,
     {
+        blockNumber: number;
         tokenAddresses: string[];
         holders: string[];
     }
 > = {
     [CHAIN_ID_MAPPING.ETHEREUM]: {
+        blockNumber: 17670778,
         tokenAddresses: [
             '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // usdc
             '0x808507121b80c02388fad14726482e061b8da827', // pendle
@@ -18,6 +20,7 @@ const TEST_DATA: Record<
         holders: ['0xa3a7b6f88361f48403514059f1f16c8e78d60eec', '0x28c6c06298d514db089934071355e5743bf21d60'],
     },
     [CHAIN_ID_MAPPING.ARBITRUM]: {
+        blockNumber: 110135034,
         tokenAddresses: [
             '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', // usdt,
             '0x0c880f6761F1af8d9Aa9C466984b80DAb9a8c9e8', // pendle
@@ -33,7 +36,7 @@ describeForChain(
         const CUR_TEST_DATA = TEST_DATA[chain];
 
         let provider: JsonRpcProvider;
-        const blockTag = 'latest';
+        const blockTag = CUR_TEST_DATA.blockNumber;
 
         const doSend = (data: Bytes) => provider.call({ data, blockTag });
         const allowPUSH0 = chain != CHAIN_ID_MAPPING.ARBITRUM;
