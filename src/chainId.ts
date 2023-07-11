8cbbfe48e89ecba2a9c7c50a1e9c06734fac323b
Tran Quang Loc <quangloctran99@gmail.com>
2023-07-11 21:07:10 +0700

    Move chainId to src

--- src/chainId.ts
+++ src/chainId.ts
@@ -1,4 +1,4 @@
 export const CHAIN_ID_MAPPING = {
     ETHEREUM: 0x1,
-    ARBITRUM: 42161,
+    ARBITRUM: 0xa4b1,
 } as const;
--- test/abi.ts
+++ test/abi.ts
@@ -3,4 +3,6 @@ export const ERC20Abi = [
     'function balanceOf(address owner) view returns (uint256)',
     'function decimals() view returns (uint8)',
     'function symbol() view returns (string)',
+    'function name() view returns (string)',
+    'function totalSupply() view returns (uint256)',
 ];
