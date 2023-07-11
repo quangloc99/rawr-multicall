5607990df66ac7bb44c2ea66610597c78d64fd15
Tran Quang Loc <quangloctran99@gmail.com>
2023-07-11 21:08:58 +0700

    Update RPC url

--- helpers/rpc-url.ts
+++ helpers/rpc-url.ts
@@ -1,6 +1,6 @@
-import { CHAIN_ID_MAPPING } from './chainId';
+import { CHAIN_ID_MAPPING } from '../src/chainId';
 
 export const RPC_URL: Record<number, string> = {
     [CHAIN_ID_MAPPING.ETHEREUM]: 'https://rpc.ankr.com/eth',
-    [CHAIN_ID_MAPPING.ARBITRUM]: 'https://rpc.ankr.com/arbitrum',
+    [CHAIN_ID_MAPPING.ARBITRUM]: 'https://arbitrum-one.publicnode.com',
 };
