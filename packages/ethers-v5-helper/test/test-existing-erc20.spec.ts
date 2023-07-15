2c97700d9077f315938ba2c3555033db6643c9bd
Tran Quang Loc <quangloctran99@gmail.com>
2023-07-15 11:53:56 +0700

    Add ethers-v5 helper test for existing erc20

---
+++ packages/ethers-v5-helper/test/test-existing-erc20.spec.ts
@@ -0,0 +1,60 @@
+import { createEthersV5Call } from '../src';
+import { buildRawMulticallContract, decodeResult, Bytes } from '@raw-multicall/core';
+import { CHAIN_ID_MAPPING, describeForChain, testData } from '@raw-multicall/test-helper';
+import { providers } from 'ethers';
+import { ERC20__factory } from '../ethers-v5-contracts/typechain-types';
+
+describeForChain(
+    (chain: number) => `buildRawMulticallContract for chain ${chain}`,
+    (rpcUrl: string, chain: number) => {
+        const CUR_TEST_DATA = testData.ERC20[chain];
+
+        let provider: providers.JsonRpcProvider;
+        const blockTag = CUR_TEST_DATA.blockNumber;
+
+        const doSend = (data: Bytes) => provider.call({ data }, blockTag);
+        const allowPUSH0 = chain != CHAIN_ID_MAPPING.ARBITRUM;
+
+        beforeAll(() => {
+            provider = new providers.JsonRpcProvider(rpcUrl);
+        });
+
+        describe('erc20', () => {
+            it('simple', async () => {
+                const contract = ERC20__factory.connect(CUR_TEST_DATA.tokenAddresses[0], provider);
+                const calls = [createEthersV5Call(contract, 'name', [])];
+                const sendData = buildRawMulticallContract(calls, { allowPUSH0 });
+                expect(sendData).toMatchSnapshot();
+                const res = await doSend(sendData.byteCode);
+                expect(res).toMatchSnapshot();
+                const output = decodeResult(calls, res);
+                expect(output).toMatchSnapshot();
+            });
+
+            it('complex', async () => {
+                const calls = await Promise.all(
+                    CUR_TEST_DATA.tokenAddresses.map((tokenAddress) => {
+                        const contract = ERC20__factory.connect(tokenAddress, provider);
+
+                        return Promise.all([
+                            createEthersV5Call(contract, 'name', []),
+                            createEthersV5Call(contract, 'symbol', []),
+                            createEthersV5Call(contract, 'decimals', []),
+                            createEthersV5Call(contract, 'totalSupply', []),
+                            ...CUR_TEST_DATA.holders.map((holder) =>
+                                createEthersV5Call(contract, 'balanceOf', [holder])
+                            ),
+                        ] as const);
+                    })
+                ).then((res) => res.flat());
+
+                const sendData = buildRawMulticallContract(calls, { allowPUSH0 });
+                expect(sendData).toMatchSnapshot();
+                const res = await doSend(sendData.byteCode);
+                expect(res).toMatchSnapshot();
+                const output = decodeResult(calls, res);
+                expect(output).toMatchSnapshot();
+            });
+        });
+    }
+);
