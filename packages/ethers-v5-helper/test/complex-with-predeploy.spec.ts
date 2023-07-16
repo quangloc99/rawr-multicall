e45e3ce452c507c4030d63c0cee60e9885284a61
Tran Quang Loc <quangloctran99@gmail.com>
2023-07-16 20:47:24 +0700

    Add complex tests

---
+++ packages/ethers-v5-helper/test/complex-with-predeploy.spec.ts
@@ -0,0 +1,115 @@
+import { describeForChain, CHAIN_ID_MAPPING } from '@raw-multicall/test-helper';
+import { ethers } from 'ethers';
+import { APlusB__factory, TestContract__factory, ThrowError__factory } from '../ethers-v5-contracts/typechain-types';
+import {
+    labeledAddress,
+    buildRawMulticallContract,
+    resetPredeployContracts,
+    registerPredeployContract,
+    strip0x,
+    decodeResult,
+    assertDefined,
+} from '@raw-multicall/core';
+import { createEthersV5Call } from '../src';
+
+describeForChain(
+    (chain: number) => `Test predeploy contracts for chain ${chain}`,
+    (rpcUrl, chain) => {
+        const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+        const APlusBFactory = new APlusB__factory();
+        const TestContractFactory = new TestContract__factory();
+        const ThrowErrorFactory = new ThrowError__factory();
+        const aPlusBContract = APlusB__factory.connect(ethers.constants.AddressZero, provider);
+        const testContract = TestContract__factory.connect(ethers.constants.AddressZero, provider);
+        const throwErrorContract = ThrowError__factory.connect(ethers.constants.AddressZero, provider);
+        const allowPUSH0 = chain !== CHAIN_ID_MAPPING.ARBITRUM;
+
+        beforeEach(() => {
+            resetPredeployContracts();
+        });
+
+        it('simple', async () => {
+            const calls = [
+                createEthersV5Call(aPlusBContract, 'plus', [10, 20], { withAddress: labeledAddress('test') }),
+                createEthersV5Call(aPlusBContract, 'minus', [10, 20], { withAddress: labeledAddress('test') }),
+            ] as const;
+            const testContractByteCode = ethers.utils.hexlify(assertDefined(APlusBFactory.getDeployTransaction().data));
+            const callData = buildRawMulticallContract(calls, {
+                allowPUSH0,
+                predeployContracts: { test: testContractByteCode },
+            });
+            expect(callData).toMatchSnapshot();
+            const res = await provider.call({ data: callData.byteCode });
+            expect(res).toMatchSnapshot();
+            expect(callData.byteCode.includes(strip0x(testContractByteCode))).toBeTruthy();
+            const result = decodeResult(calls, res);
+            expect(result).toMatchSnapshot();
+        });
+
+        it('complex', async () => {
+            const aPlusBContractByteCode = ethers.utils.hexlify(
+                assertDefined(APlusBFactory.getDeployTransaction().data)
+            );
+            const testContractByteCode = ethers.utils.hexlify(
+                assertDefined(TestContractFactory.getDeployTransaction().data)
+            );
+
+            const calls = [
+                createEthersV5Call(aPlusBContract, 'plus', [1, 2], { withAddress: labeledAddress('a') }),
+                createEthersV5Call(aPlusBContract, 'plus', [3, 4], { withAddress: labeledAddress('b') }),
+                createEthersV5Call(aPlusBContract, 'minus', [5, 6], { withAddress: labeledAddress('c') }),
+
+                createEthersV5Call(testContract, 'compare', [1, 2], { withAddress: labeledAddress('x') }),
+                createEthersV5Call(testContract, 'swapXY', [{ x: 1, y: 2 }], {
+                    withAddress: labeledAddress('y'),
+                }),
+                createEthersV5Call(testContract, 'hash', [1], { withAddress: labeledAddress('z') }),
+            ];
+
+            const callData = buildRawMulticallContract(calls, {
+                allowPUSH0,
+                predeployContracts: {
+                    a: aPlusBContractByteCode,
+                    b: aPlusBContractByteCode,
+                    c: aPlusBContractByteCode,
+                    x: testContractByteCode,
+                    y: testContractByteCode,
+                    z: testContractByteCode,
+                },
+            });
+
+            expect(callData).toMatchSnapshot();
+            const res = await provider.call({ data: callData.byteCode });
+            const decodedResult = decodeResult(calls, res);
+            expect(res).toMatchSnapshot();
+            expect(decodedResult).toMatchSnapshot();
+        });
+
+        it('throw error', async () => {
+            const throwErrorContractByteCode = ethers.utils.hexlify(
+                assertDefined(ThrowErrorFactory.getDeployTransaction().data)
+            );
+            registerPredeployContract('throw-error', throwErrorContractByteCode);
+            const calls = [
+                createEthersV5Call(throwErrorContract, 'justRevert', ['abc'], {
+                    withAddress: labeledAddress('throw-error'),
+                }),
+                createEthersV5Call(throwErrorContract, 'revertError', ['xyz'], {
+                    withAddress: labeledAddress('throw-error'),
+                }),
+                createEthersV5Call(throwErrorContract, 'revertCustom', [1], {
+                    withAddress: labeledAddress('throw-error'),
+                }),
+                createEthersV5Call(throwErrorContract, 'revertPanic', [], {
+                    withAddress: labeledAddress('throw-error'),
+                }),
+            ] as const;
+
+            const calldata = buildRawMulticallContract(calls, { allowPUSH0 });
+            expect(calldata).toMatchSnapshot();
+            const res = await provider.call({ data: calldata.byteCode });
+            const decodedRes = decodeResult(calls, res);
+            expect(decodedRes).toMatchSnapshot();
+        });
+    }
+);
